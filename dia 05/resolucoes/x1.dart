6f8d9dbc5d107ddcf30faa0ccc7ec29f71fd16e2
RFS-Junior <juniorferreira59@outlook.com>
2023-07-29 11:13:20 -0300

    add obs para exercicio 3

--- dia 05/resolucoes/x1.dart
+++ dia 05/resolucoes/x1.dart
@@ -12,6 +12,8 @@ Future<void> main() async {
     List<String> nomes = event.map((nome) => nome).toList();
     // Imprime todos os nomes alocados no evento atual
     print(nomes);
+    // OBS: Se quiser imprimir o tamanho (em catacteres) de cada nome basta colocar .length
+    //List<int> lenghNomes = event.map((nome) => nome.length).toList();
   });
 
   // Adiciona um evento com os nomes
