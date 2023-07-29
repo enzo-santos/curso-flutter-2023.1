f3c94dd2febd1a7bcb6a6f710e43097d2dd16f4b
RFS-Junior <juniorferreira59@outlook.com>
2023-07-29 10:57:29 -0300

    add bonus dia 05

--- dia 05/resolucoes/e4.dart
+++ dia 05/resolucoes/e4.dart
@@ -1,30 +1,14 @@
 import 'dart:async';
 
-Future<void> main() async {
-  // Define um controlador de stream
-  StreamController<List<String>> streamController =
-      StreamController<List<String>>();
+void main() {
+  final List<String> nomes = ["João", "Maria", "José", "Pedro", "Ana"];
 
-  //Ouve qualquer evento lançado na stream
-  streamController.stream.listen((event) {
-    // Pega todos os nomes de cada evento lançado
-    List<String> lengthNomes = event.map((nome) => nome).toList();
-    // Imprime
-    print(lengthNomes);
+  Stream<List<String>> nomesStream = Stream.value(nomes);
+
+  Stream<List<int>> lengthNomes =
+      nomesStream.map((nomes) => nomes.map((nome) => nome.length).toList());
+
+  lengthNomes.toList().then((lengthsList) {
+    print('Comprimentos dos nomes: $lengthsList');
   });
-
-  // Adiciona um evento com os nomes
-  streamController.sink.add(["Joao", "Maria", "Jose", "Pedro", "Ana"]);
-
-  // Bloqueia a execução por 3 segundos
-  await Future.delayed(const Duration(seconds: 3));
-
-  // Adicionar um outro evento com novos nomes
-  streamController.sink.add(["Marcia", "Beto", "Patricia", "Maria", "Marcos"]);
-
-  // Bloqueia a execução por 2 segundos - Adicionar um outro evento com novos nomes
-  await Future.delayed(
-      const Duration(seconds: 2),
-      () => streamController.sink
-          .add(["Felipe", "Thiago", "Victor", "Larissa", "Thais"]));
 }
